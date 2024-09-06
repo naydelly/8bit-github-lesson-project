d07d6efaa381d66fc5956652a05beb846d9a9db7
Naydel Espiritu <naydele@dhcp-168-105-29-169.wireless.manoa.hawaii.edu>
2024-09-06 12:40:02 -1000

    Revert "created foobar class"
    This reverts commit aca2b3ff4f170558a9e9baf7558c04d0fc88cd2e.

--- FooBar.java
+++
@@ -1,9 +0,0 @@
-public class FooBar{ 
-
-    public static void main(String[] args) {
-    
-        System.out.println("FooBar"); 
-    
-    } 
-    
-}
--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
     
-        System.out.println("Bye, World!"); 
+    System.out.println("Hello, World!!"); 
     
     } 
 }
